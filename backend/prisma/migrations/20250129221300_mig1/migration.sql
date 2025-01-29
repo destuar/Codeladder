b6fa467ae849b902d5250c9336aa05da869111b8
Chris Beaudoin <chrisbeaudoin.school@gmail.com>
2025-01-29 14:20:23 -0800

    updated prism migration.

---
+++ backend/prisma/migrations/20250129221300_mig1/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "tokenVersion" INTEGER NOT NULL DEFAULT 0,
+    "avatar" TEXT,
+    "bio" TEXT,
+    "location" TEXT,
+    "website" TEXT,
+    "phoneNumber" TEXT,
+    "dateOfBirth" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
